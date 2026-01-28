fc89c980590e2da5e42736cba0fba1d9c2c71a5b
Tu Nombre <tu.email@example.com>
2026-01-28 17:25:41 -0500

    Cambios generales 2

--- js/load-menu.js
+++ js/load-menu.js
@@ -21,6 +21,7 @@ function cargarMenu() {
                     <a href="index.html" ${paginaActual === 'index.html' ? 'class="active" style="color: var(--color-cyan);"' : ''}>Inicio</a>
                     <a href="institucion.html" ${paginaActual === 'institucion.html' ? 'class="active" style="color: var(--color-cyan);"' : ''}>Institución</a>
                     <a href="metodologia.html" ${paginaActual === 'metodologia.html' ? 'class="active" style="color: var(--color-cyan);"' : ''}>Metodología</a>
+                    <a href="galeria.html" ${paginaActual === 'galeria.html' ? 'class="active" style="color: var(--color-cyan);"' : ''}>Galería</a>
                     <a href="programas.html" ${paginaActual === 'programas.html' ? 'class="active" style="color: var(--color-cyan);"' : ''}>Programas</a>
                     <a href="orientacion.html" ${paginaActual === 'orientacion.html' ? 'class="active" style="color: var(--color-cyan);"' : ''}>Orientación Vocacional</a>
                     <a href="https://preuniversitario.fundel.com.ec/" target="_blank" style="color: var(--color-yellow); font-weight: 600;">Aulas Digitales</a>
